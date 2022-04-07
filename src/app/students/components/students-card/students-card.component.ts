c168f08790f7988d5a22fca99e968499836602f2
LaietiMcLean <laia.marco@bootcamp-upgrade.com>
2022-04-07 14:19:02 +0200

    Angular Material + db.json

---
+++ src/app/students/components/students-card/students-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-students-card',
+  templateUrl: './students-card.component.html',
+  styles: [
+  ]
+})
+export class StudentsCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/students/students.module.ts
+++ src/app/students/students.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
 
 import { StudentsRoutingModule } from './students-routing.module';
+import { MaterialModule } from '../material/material.module';
 
 import { AddComponent } from './pages/add/add.component';
 import { SearchComponent } from './pages/search/search.component';
 import { StudentsComponent } from './pages/students/students.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListComponent } from './pages/list/list.component';
+import { StudentsCardComponent } from './components/students-card/students-card.component';
 
 
 @NgModule({
@@ -16,10 +20,14 @@ import { ListComponent } from './pages/list/list.component';
     SearchComponent,
     StudentsComponent,
     HomeComponent,
-    ListComponent
+    ListComponent,
+    StudentsCardComponent
   ],
   imports: [
     CommonModule,
+    FlexLayoutModule,
+    FormsModule,
+    MaterialModule,
     StudentsRoutingModule
   ]
 })
