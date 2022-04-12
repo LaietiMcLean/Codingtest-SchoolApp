2d6f0299e55bbe34d89be4554b6b1f961a9b051a
LaietiMcLean <laia.marco@bootcamp-upgrade.com>
2022-04-12 13:06:39 +0200

    some changes on db.json

--- src/app/shared/error-page/error-page.component.ts
+++ src/app/shared/error-page/error-page.component.ts
@@ -3,7 +3,37 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-error-page',
   templateUrl: './error-page.component.html',
-  styles: [
+  styles: [`
+      a {
+        text-decoration: none;
+        padding: 2px 1px 0;
+      }
+      a:link {
+      color: #265301;
+      }
+
+      a:visited {
+        color: #437A16;
+      }
+
+      a:focus {
+        background: #BAE498;
+      }
+
+      a:hover {
+        background: #CDFEAA;
+      }
+
+      h1 {
+        font-weight: bold;
+        margin: 20px;
+      }
+
+      li {
+        list-style: none;
+        margin: 20px;
+      }
+  `
   ]
 })
 export class ErrorPageComponent implements OnInit {
